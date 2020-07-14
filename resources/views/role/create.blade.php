976ecb0543a5c002c18c164c85d79554650e0a04
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-07-13 23:35:44 -0500

    Uso de old text para datos de formulario

--- resources/views/role/create.blade.php
+++ resources/views/role/create.blade.php
@@ -20,24 +20,39 @@
 
                         <div class="form-group">
                             <input type="text" class="form-control" id="name" placeholder="Name"
-                            name="name">
+                            name="name"
+                            value="{{ old('name') }}">
                         </div>
                         <div class="form-group">
                             <input type="text" class="form-control" id="slug" placeholder="Slug"
-                            name="slug">
+                            name="slug"
+                            value="{{ old('slug') }}">
                         </div>
                           
                         <div class="form-group">
-                            <textarea class="form-control" id="description" name="description" rows="3" placeholder="Description"></textarea>
+                            <textarea class="form-control" id="description" name="description" rows="3" placeholder="Description"
+                            >{{ old('description') }}</textarea>
                         </div>
                         <hr>
                         <h3>Full Access</h3>
                         <div class="custom-control custom-radio custom-control-inline">
-                            <input type="radio" id="fullaccessyes" name="full-access" class="custom-control-input" value="yes">
+                            <input type="radio" id="fullaccessyes" name="full-access" class="custom-control-input" value="yes"
+                            @if (old('full-access') == "yes")
+                                checked
+                            @endif
+                            
+                            >
                             <label class="custom-control-label" for="fullaccessyes">Yes</label>
                           </div>
                           <div class="custom-control custom-radio custom-control-inline">
-                            <input type="radio" id="fullaccessno" name="full-access" class="custom-control-input" value="no" checked>
+                            <input type="radio" id="fullaccessno" name="full-access" class="custom-control-input" value="no" 
+                            @if (old('full-access') == "no")
+                             checked
+                            @endif
+                            @if (old('full-access') === null)
+                             checked
+                            @endif
+                            >
                             <label class="custom-control-label" for="fullaccessno">No</label>
                           </div>
 
@@ -50,6 +65,10 @@
                         <input type="checkbox" class="custom-control-input" id="permission_{{$permission->id}}"
                         value="{{$permission->id}}"
                         name="permission[]"
+
+                        @if (is_array(old('permission')) && in_array("$permission->id", old('permission')))
+                                checked
+                         @endif
                         >
                             <label class="custom-control-label" for="permission_{{$permission->id}}">{{ $permission->id }}
                             - 
@@ -60,8 +79,6 @@
                         @endforeach
                         <hr>
                         <input type="submit" value="Guardar" class="btn btn-primary">
-
-                        
                     </div>
                     </form>
                 </div>
